b508234f2f8365d9a4be8fa98753de48d6a8b97b
Zach Toups <ztoups@nmsu.edu>
2017-01-26 11:08:54 -0700

    I'm not paying attention to anyone else just editing away

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
+		System.out.println("I am working here and not communicating with my teams........");
 		System.out.println("here's one from the command line!");
 		System.out.println("I'm using sourcetree.");
 	}
