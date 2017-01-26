88153e536a32b6361c63ea618e3ed1441e69d652
Zach Toups <ztoups@nmsu.edu>
2017-01-26 11:15:00 -0700

    added vim hate

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("I am working here and not communicating with my teams........");
+		System.out.println("I love emacs and I hate vim.");
+		System.out.println("I am working here and not communicating with my teams.");
 		System.out.println("here's one from the command line!");
 		System.out.println("I'm using sourcetree.");
 	}
